abed1b7341fe8e9f7ed8c18c3d3545724fe89171
Aditya-Raj143 <adityaxraj72004@gmail.com>
2024-12-25 00:03:08 +0530

    added zod schemas for several components, added database connection

---
+++ src/schemas/signUpSchema.ts
@@ -0,0 +1,17 @@
+import { z } from "zod";
+
+export const usernameValidation = z
+  .string()
+  .min(4, "Username must be atleast 2 characters")
+  .max(20, "Username must not exceed 20 characters")
+  .regex(/^[a-zA-Z0-9_]+$/, "Username must not contain special charcters");
+
+export const signupSchema = z.object({
+  username: usernameValidation,
+  email: z.string().email({ message: "Inavlid email address" }),
+  password: z
+    .string()
+    .min(6, { message: "password must be atleast 6 characters" }),
+});
+
+
