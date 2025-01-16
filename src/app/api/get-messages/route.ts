b8cef2fa560a10391f7e0ff4f4b48e0917e251da
Aditya-Raj143 <adityaxraj72004@gmail.com>
2025-01-16 19:53:20 +0530

    added route for sending and receiving messages

---
+++ src/app/api/get-messages/route.ts
@@ -0,0 +1,68 @@
+import dbConnect from "@/lib/dbConnect";
+import { getServerSession } from "next-auth";
+import UserModel from "@/model/User.model";
+import { authOptions } from "../auth/[...nextauth]/options";
+import mongoose from "mongoose";
+
+export async function GET(request: Request) {
+  await dbConnect();
+
+  const session = await getServerSession(authOptions);
+
+  if (!session || !session.user) {
+    return Response.json(
+      {
+        success: false,
+        message: "Not authenticated",
+      },
+      { status: 401 }
+    );
+  }
+  const user = session?.user;
+
+  const userId = new mongoose.Types.ObjectId(user._id);
+
+  try {
+    const user = await UserModel.aggregate([
+      {
+        $match: { id: userId },
+      },
+      {
+        $unwind: "$messages",
+      },
+      {
+        $sort: { "messages.createdAt": 1 },
+      },
+      {
+        $group: { _id: "$_id", messages: { $push: "$messages" } },
+      },
+    ]);
+
+    if (!user || user.length === 0) {
+      return Response.json(
+        {
+          success: false,
+          message: "User not found",
+        },
+        { status: 401 }
+      );
+    }
+
+    return Response.json(
+      {
+        success: true,
+        messages: user[0].messages,
+      },
+      { status: 200 }
+    );
+  } catch (error) {
+    console.log("An unexpeceted error occured ", error);
+    return Response.json(
+      {
+        success: false,
+        message: "Unexpected error occured",
+      },
+      { status: 500 }
+    );
+  }
+}
