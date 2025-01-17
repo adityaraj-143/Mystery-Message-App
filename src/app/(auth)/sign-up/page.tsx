25a1a129381f100060273631fdded1a8eba64bfd
Aditya-Raj143 <adityaxraj72004@gmail.com>
2025-01-17 20:24:17 +0530

    minor bug fix

--- src/app/(auth)/sign-up/page.tsx
+++ src/app/(auth)/sign-up/page.tsx
@@ -118,7 +118,7 @@ const page = () => {
                   </FormControl>
                   {ischeckingUsername && <Loader2 className="animate-spin" />}
                   <p className={`text-sm ${usernameMessage === "Username is unique" ? "text-green-500" : "text-red-500"}`}>
-                    test {usernameMessage}
+                    {usernameMessage}
                   </p>
                   <FormMessage />
                 </FormItem>
